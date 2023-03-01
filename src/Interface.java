bc91300186117161ab6484fb4c7de7e33c6c5d5d
Omar Garcia M <ofgmora@gmail.com>
2023-02-28 18:32:26 -0700

    Usando interface

--- src/Interface.java
+++ src/Interface.java
@@ -1,11 +1,23 @@
 public class Interface {
 
     public static void main(String[] args) {
+        Avion avion = new Avion();
+        Avioneta avioneta = new Avioneta();
 
+        EjecutaDespegar(avioneta);
+        EjecutaAterrizar(avion);
+    }
+
+    public static void EjecutaDespegar(Volador volador) {
+        volador.Despegar(50);
+    }
+
+    public static void EjecutaAterrizar(Volador volador) {
+        volador.Aterrizar(20);
     }
 }
 
-//Interface:, las utilizamos cuando queremos crear clases y esas clases tengan funciones prev definidas
+//Interface:, las utilizamos cuando queremos crear clases y esas clases tengan funciones/métodos prev definidas
 interface Volador{
     void Despegar(int cuantaVelocidad);
     void Aterrizar(int cuantaVelocidad);
@@ -15,11 +27,24 @@ class Avion implements Volador {
 
     @Override
     public void Despegar(int cuantaVelocidad) {
-
+        System.out.println("Avion() -> Acelerar()");
     }
 
     @Override
     public void Aterrizar(int cuantaVelocidad) {
+        System.out.println("Avion() -> Frenar()");
+    }
+}
 
+class Avioneta implements Volador {
+
+    @Override
+    public void Despegar(int cuantaVelocidad) {
+        System.out.println("Avioneta() -> Acelerar()");
+    }
+
+    @Override
+    public void Aterrizar(int cuantaVelocidad) {
+        System.out.println("Avioneta() -> Frenar()");
     }
 }
